dd0e2c1894fd85e302cf0424bdbf48a6febaf52f
crealu <cameron.miller294@gmail.com>
2023-02-23 17:51:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,45 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-	const output = `
-		<p>New Membership</p>
-		<ul>
-			test
-		</ul>
-	`;
+// app.get('/', (req, res) => {
+// 	const output = `
+// 		<p>New Membership</p>
+// 		<ul>
+// 			test
+// 		</ul>
+// 	`;
 
+// 	let smtpConfig = {
+// 		host: 'smtp.gmail.com',
+// 		port: 465,
+// 		secure: true,
+// 		auth: {
+// 			user: process.env.THE_EMAIL,
+// 			pass: process.env.THE_KEY
+// 		},
+// 	}
+
+// 	let transporter = nodemailer.createTransport(smtpConfig);
+
+// 	let mailOptions = {
+// 		from: 'Niheigo <niheigodev@site.com>',
+// 		to: 'niheigodev@gmail.com',
+// 		subject: 'Membership Request',
+// 		text: 'text',
+// 		html: output
+// 	};
+
+// 	transporter.sendMail(mailOptions, (err, info) => {
+// 		if (err) { return console.log(err) }
+// 		console.log(info);
+// 	})
+
+// 	res.send('Server running');
+// });
+
+app.post('/send-member', (req, res) => {
+	const ouput = 'test';
+	
 	let smtpConfig = {
 		host: 'smtp.gmail.com',
 		port: 465,
@@ -22,7 +53,7 @@ app.get('/', (req, res) => {
 		auth: {
 			user: process.env.THE_EMAIL,
 			pass: process.env.THE_KEY
-		},
+		}
 	}
 
 	let transporter = nodemailer.createTransport(smtpConfig);
@@ -38,9 +69,9 @@ app.get('/', (req, res) => {
 	transporter.sendMail(mailOptions, (err, info) => {
 		if (err) { return console.log(err) }
 		console.log(info);
-	})
+	});
 
-	res.send('Server running');
-});
+	res.send('Membership submitted');
+})
 
 app.listen(port, () => console.log('listening on 4400'));
