05e722c9c8b3322282533c22a3ee6628c0cbbf89
crealu <cameron.miller294@gmail.com>
2023-02-23 20:44:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,41 +7,9 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
-// app.get('/', (req, res) => {
-// 	const output = `
-// 		<p>New Membership</p>
-// 		<ul>
-// 			test
-// 		</ul>
-// 	`;
-
-// 	let smtpConfig = {
-// 		host: 'smtp.gmail.com',
-// 		port: 465,
-// 		secure: true,
-// 		auth: {
-// 			user: process.env.THE_EMAIL,
-// 			pass: process.env.THE_KEY
-// 		},
-// 	}
-
-// 	let transporter = nodemailer.createTransport(smtpConfig);
-
-// 	let mailOptions = {
-// 		from: 'Niheigo <niheigodev@site.com>',
-// 		to: 'niheigodev@gmail.com',
-// 		subject: 'Membership Request',
-// 		text: 'text',
-// 		html: output
-// 	};
-
-// 	transporter.sendMail(mailOptions, (err, info) => {
-// 		if (err) { return console.log(err) }
-// 		console.log(info);
-// 	})
-
-// 	res.send('Server running');
-// });
+app.get('/', (req, res) => {
+	res.send('Server running');
+});
 
 app.post('/send-member', (req, res) => {
 	const ouput = 'test';
