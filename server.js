31aa521cbb03202012403c93a45d7c36d71679e6
crealu <cameron.miller294@gmail.com>
2023-03-08 18:39:23 -0500

    Update form
    - Added names for form inputs
    - Created success.html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const port = 4400 || process.env.PORT;
-require('dotenv').config({ path: '.env' });
+// require('dotenv').config({ path: '.env' });
 
 const app = express();
 
@@ -13,8 +13,49 @@ app.get('/', (req, res) => {
 });
 
 app.post('/send-member', (req, res) => {
-	// console.log(req.body);
-	const output = 'test';
+	console.log(req.body);
+	const output = `
+		<h3>New Membership Request</h3>
+		<p>
+			Submittal Date: ${req.body.submittalDate}<br/>
+			Precinct #: ${req.body.precinct}<br/>
+			Name: ${req.body.name}<br/>
+			Email: ${req.body.email}<br/>
+			Address: ${req.body.address}<br/>
+			Employer: ${req.body.employer}<br/>
+			Occupation: ${req.body.occupation}<br/>
+			Recruited By: ${req.body.recruitedBy}<br/>
+		</p>
+
+		<p>Methods of Contact:</p>
+		<ul>
+			${req.body.mailContact == 'on' ? '<li>Mail</li>' : ''}
+			${req.body.emailContact == 'on' ? '<li>Email</li>' : ''}
+			${req.body.phoneContact == 'on' ? '<li>Phone</li>' : ''}
+		</ul>
+
+		<p>Interested in:</p>
+		<ul>
+			${req.body.memI == 'on' ? '<li>Membership</li>' : ''}
+			${req.body.nomI == 'on' ? '<li>Nominating</li>' : ''}
+			${req.body.issueAdvI == 'on' ? '<li>Issue Advocacy</li>' : ''}
+			${req.body.commServI == 'on' ? '<li>Community Service</li>' : ''}
+			${req.body.legisI == 'on' ? '<li>Legislative</li>' : ''}
+			${req.body.legalI == 'on' ? '<li>Legal</li>' : ''}
+			${req.body.commI == 'on' ? '<li>Communications</li>' : ''}
+			${req.body.fundI == 'on' ? '<li>Fundraising</li>' : ''}
+			${req.body.voterRegI == 'on' ? '<li>Voter Registration</li>' : ''}
+		</ul>
+
+		<p>Payment by: </p>
+		<ul>
+			${req.body.duesCash == 'on' ? '<li>Cash</li>' : ''}
+			${req.body.duesCheck == 'on' ? '<li>Check</li>' : ''}
+		</ul>
+
+		<p>Signature: ${req.body.signature}</p>
+		<p>Date: ${req.body.date}</p>
+	`;
 	
 	let smtpConfig = {
 		host: 'smtp.gmail.com',
@@ -41,8 +82,7 @@ app.post('/send-member', (req, res) => {
 		console.log(info);
 	});
 
-	res.send('received');	
-	res.redirect('/');
+	res.sendFile('success.html', { root: './' });
 })
 
 app.listen(port, () => console.log('listening on 4400'));
